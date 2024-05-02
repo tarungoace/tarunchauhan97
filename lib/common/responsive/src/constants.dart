7848de527c196d803b80e697e66371d7a404656a
tarun <chauhan.tarun897@gmail.com>
2024-05-02 20:00:10 +0530

    added assets

--- lib/common/responsive/src/constants.dart
+++ lib/common/responsive/src/constants.dart
@@ -5,3 +5,21 @@ const double kMinLargeTabletWidth = 720;
 const double kMinSmallDesktopWidth = 1024;
 const double kMinMediumDesktopWidth = 1440;
 const double kMinLargeDesktopWidth = 1920;
+
+class ProjectDataValue {
+  final String? imageAsset;
+  final String? titleProject;
+  final String? githubLink;
+  final String? playStoreLink;
+  final String? appStoreLink;
+  final String? websiteLink;
+
+  ProjectDataValue(
+    this.githubLink,
+    this.playStoreLink,
+    this.appStoreLink,
+    this.websiteLink,
+    this.imageAsset,
+    this.titleProject,
+  );
+}
--- lib/config/constants/assets_paths.dart
+++ lib/config/constants/assets_paths.dart
@@ -1,9 +1,9 @@
-const kMobileDevelopmentBackgroundPath = 'assets/images/mobile_development_bk.jpg';
+const kMobileDevelopmentBackgroundPath =
+    'assets/images/mobile_development_bk.jpg';
 const kHomeBackgroundImagePath = 'assets/images/background1.jpg';
 const kMyPictureImagePath = 'assets/images/my_picture2.jpg';
 
 const kMobileDevelopmentIconPath = 'assets/icons/mobile-development.svg';
-
 const kEnglishFlagRoundedSvgPath = 'assets/icons/english_flag_rounded.svg';
 const kIndiaFlagRoundedSvgPath = 'assets/icons/india_flag.svg';
 
@@ -12,5 +12,12 @@ const kLinkedinSvgPath = 'assets/icons/linkedin.svg';
 const kInstagramSvgPath = 'assets/icons/instagram.svg';
 const kTelegramSvgPath = 'assets/icons/telegram.svg';
 
-
 const meeImage = "assets/images/mee.jpg";
+
+const String piGemsHubBanner = 'assets/images/pigemshub1.jpg';
+
+const String learningBanner = 'assets/images/learning1.png';
+
+const String virtDoctor = 'assets/images/virtdoctor.png';
+
+const String foodOnDel = 'assets/images/foodondel.png';
